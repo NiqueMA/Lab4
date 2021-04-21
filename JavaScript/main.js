9fe3891ab3e8239ff012aa6d4347b1a1329335c4
NiqueMA <77253680+NiqueMA@users.noreply.github.com>
2021-04-20 20:20:13 -0400

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,17 @@
+function openNav() 
+{
+    document.getElementById("myNav").style.height = "100%";
+}
+  
+  function closeNav() 
+{
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
